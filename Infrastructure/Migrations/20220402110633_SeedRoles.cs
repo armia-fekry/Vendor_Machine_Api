42350031e55534487b191286f0fa9ebcccc79f32
Armia Fekry <Armia.Fekry@InteractTS.com>
2022-04-02 13:57:37 +0200

    add migrations

---
+++ Infrastructure/Migrations/20220402110633_SeedRoles.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+using System;
+
+namespace JWTNET_5.Infrastructure.Migrations
+{
+    public partial class SeedRoles : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.InsertData(
+                            table: "AspNetRoles",
+                            columns: new[] { "Id", "Name", "NormalizedName", "ConcurrencyStamp" },
+                            values: new object[] { Guid.NewGuid().ToString(), "Buyer", "Buyer".ToUpper(), Guid.NewGuid().ToString() }
+                        );
+
+                    migrationBuilder.InsertData(
+                        table: "AspNetRoles",
+                        columns: new[] { "Id", "Name", "NormalizedName", "ConcurrencyStamp" },
+                        values: new object[] { Guid.NewGuid().ToString(), "Seller", "Seller".ToUpper(), Guid.NewGuid().ToString() }
+                    );
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.Sql("DELETE FROM [AspNetRoles]");
+
+        }
+    }
+}
